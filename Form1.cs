a2155f310a8e66734bfd1a1e14787a4e1fc5c3f1
Will <twitchifygamers@gmail.com>
2021-12-16 13:01:08 +0000

    Removed comment

--- Form1.cs
+++ Form1.cs
@@ -56,7 +56,7 @@ namespace CardsAgainstSoftwareDevelopment
             randomList.Add("My program is not working! ");
             randomList.Add("What am I supposed to do with this error? ");
 
-            //sentence.Text = new Random(randomList);
+            sentence.Text = new Random(randomList[amount]);
         }
     }
 }
