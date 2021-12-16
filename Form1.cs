8264b645be75cab2f223bc244173475b8f6ca86d
Will <twitchifygamers@gmail.com>
2021-12-16 15:38:15 +0000

    Cards update sentence now

--- Form1.cs
+++ Form1.cs
@@ -19,32 +19,32 @@ namespace CardsAgainstSoftwareDevelopment
 
         private void card1_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card1.Text;
         }
 
         private void card2_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card2.Text;
         }
 
         private void card3_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card3.Text;
         }
 
         private void card4_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card4.Text;
         }
 
         private void card5_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card5.Text;
         }
 
         private void card6_Click(object sender, EventArgs e)
         {
-
+            sentence.Text += card6.Text;
         }
 
         private void sentence_Click(object sender, EventArgs e)
@@ -55,6 +55,8 @@ namespace CardsAgainstSoftwareDevelopment
 
             randomList.Add("My program is not working! ");
             randomList.Add("What am I supposed to do with this error? ");
+
+            sentence.Text = "My program is not working! ";
         }
     }
 }
